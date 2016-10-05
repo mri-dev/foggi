5b224f2494cbf2889fcaffc1c3319840540a0c9f
mistvan22 <pocokxtrame@gmail.com>
2016-10-05 21:39:32 +0200

    Keretrendszer előkészítés, clearup

---
+++ shop/settings/data.php
@@ -0,0 +1,13 @@
+<?
+	define("DB_HOST",'localhost');
+	define("DB_NAME",'mridevco_foggi');
+	define("DB_USER",'mridevco_admin');
+	define("DB_PW",'MoIst1991');
+	
+	// SMTP Settings
+	define("SMTP_HOST",	"");
+	define("SMTP_USER",	"");
+	define("SMTP_PW",	"");
+	define("SMTP_MODE",	"ssl");
+	define("SMTP_PORT",	465);
+?>
